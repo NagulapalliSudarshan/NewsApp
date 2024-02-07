7f7bf6757b80e8e72b6856dd829eb83dfa12eee4
sudarshan <sudarshan.nagulapalli@gmail.com>
2024-02-07 23:26:02 +0530

    small changes

--- src/Components/News.js
+++ src/Components/News.js
@@ -2,15 +2,26 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export default class News extends Component {
-  render() {
-    return (
-      <div>
-        <h1>This is a new comp</h1>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-      </div>
-    )
-  }
+    constructor(){
+        super();
+        
+      }
+    render() {
+        return (
+        <div className='container my-3'>
+            <h3>Today's Top News</h3>
+            <div className='row'>
+                <div className='col-md-4'>
+                    <NewsItem url="..." title="hi" description="desc"/>
+                </div>
+                <div className='col-md-4'>
+                    <NewsItem url="..." title="hi" description="desc"/>
+                </div>
+                <div className='col-md-4'>
+                    <NewsItem url="..." title="hi" description="desc"/>
+                </div>
+            </div>
+        </div>
+        )
+    }
 }
