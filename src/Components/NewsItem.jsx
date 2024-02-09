fc7838529bf6d61925b8c79c8af100bd047620e4
sudarshan <sudarshan.nagulapalli@gmail.com>
2024-02-09 10:13:21 +0530

    'commits'

--- src/Components/NewsItem.jsx
+++ src/Components/NewsItem.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-const img = "https://img.freepik.com/premium-photo/news-broadcast-background-breaking-news_327072-2662.jpg";
+const img = "https://media.istockphoto.com/id/1181778359/vector/breaking-news-vector-background.jpg?s=612x612&w=0&k=20&c=jPg0fAK-Itq51pmv_sRp7UspRhQNY-H_3h8OSB85KgE=";
 
 export default class NewsItem extends Component {
   render() {
@@ -8,7 +8,7 @@ export default class NewsItem extends Component {
     return (
       <div className='my-2'>
         <div className="card" style={{"width": "23rem"}}>
-          <img src={imgUrl?imgUrl:img} className="card-img-top" alt="..." />
+          <img src={imgUrl?imgUrl:img} className="card-img-top" alt="" />
           <div className="card-body">
             <h5 className="card-title my-2">{title?title.slice(0,50)+"..":" "}</h5>
             <p className="card-text">{description?description.slice(0,70)+"...":" "}</p>
